8021d9b814bb0a4ec31729fa88582258b93fa06c
siamkarl <77637133+siamkarl@users.noreply.github.com>
2025-03-17 09:19:52 +0100

    Add files via upload

--- admin_skola.sql
+++ admin_skola.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Mar 15, 2025 at 07:27 PM
--- Server version: 10.6.20-MariaDB
--- PHP Version: 8.3.14
+-- Värd: localhost:3306
+-- Tid vid skapande: 17 mars 2025 kl 09:19
+-- Serverversion: 10.6.20-MariaDB
+-- PHP-version: 8.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `admin_skola`
+-- Databas: `admin_skola`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookings`
+-- Tabellstruktur `bookings`
 --
 
 CREATE TABLE `bookings` (
@@ -39,7 +39,7 @@ CREATE TABLE `bookings` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `maintenance`
+-- Tabellstruktur `maintenance`
 --
 
 CREATE TABLE `maintenance` (
@@ -50,7 +50,7 @@ CREATE TABLE `maintenance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `maintenance`
+-- Dumpning av Data i tabell `maintenance`
 --
 
 INSERT INTO `maintenance` (`id`, `printer`, `start_date`, `end_date`) VALUES
@@ -59,7 +59,7 @@ INSERT INTO `maintenance` (`id`, `printer`, `start_date`, `end_date`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `printers`
+-- Tabellstruktur `printers`
 --
 
 CREATE TABLE `printers` (
@@ -71,20 +71,27 @@ CREATE TABLE `printers` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `print_jobs`
+-- Tabellstruktur `print_jobs`
 --
 
 CREATE TABLE `print_jobs` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `progress` int(11) NOT NULL,
-  `user` varchar(255) NOT NULL
+  `user` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumpning av Data i tabell `print_jobs`
+--
+
+INSERT INTO `print_jobs` (`id`, `name`, `progress`, `user`) VALUES
+(2, 'Namnskyltar - Skuggare o Prao ', 10, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `support_requests`
+-- Tabellstruktur `support_requests`
 --
 
 CREATE TABLE `support_requests` (
@@ -96,10 +103,17 @@ CREATE TABLE `support_requests` (
   `status` varchar(50) DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumpning av Data i tabell `support_requests`
+--
+
+INSERT INTO `support_requests` (`id`, `user`, `printer`, `issue`, `response`, `status`) VALUES
+(3, 'Siam.karlsson', 'Prusa Mk4S', 'Hej! Detta Ã¤r ett test', 'Test', 'In Progress');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `unavailable_dates`
+-- Tabellstruktur `unavailable_dates`
 --
 
 CREATE TABLE `unavailable_dates` (
@@ -108,7 +122,7 @@ CREATE TABLE `unavailable_dates` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `unavailable_dates`
+-- Dumpning av Data i tabell `unavailable_dates`
 --
 
 INSERT INTO `unavailable_dates` (`id`, `date`) VALUES
@@ -118,7 +132,7 @@ INSERT INTO `unavailable_dates` (`id`, `date`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Tabellstruktur `users`
 --
 
 CREATE TABLE `users` (
@@ -132,103 +146,103 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `users`
+-- Dumpning av Data i tabell `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `email`, `token`, `verified`) VALUES
-(1, 'admin', '$2y$10$Z82A0tAEq3.BuJI0ibVQlO2SjADTy1gpaPlw7WIWbLMnS4LXNafaq', 'admin', 'siam.karlsson@gmail.com', '9f8d8206dd15372ef4f393f28e636cbd', 1),
-(2, 'siam.karlsson', '$2y$10$0R1BwNJqnVJiSZcyff.H1uR7FZByNJjh6zlFxM7lG8befFAVtw8Lm', 'user', 'siam.karlsson@elev.ga.ntig.se', '7eff3ea5f7e2e4e8f7b3355aff3a94be', 1);
+(1, 'admin', '$2y$10$0R1BwNJqnVJiSZcyff.H1uR7FZByNJjh6zlFxM7lG8befFAVtw8Lm', 'admin', 'siam.karlsson@gmail.com', '9f8d8206dd15372ef4f393f28e636cbd', 1),
+(2, 'user', '$2y$10$0R1BwNJqnVJiSZcyff.H1uR7FZByNJjh6zlFxM7lG8befFAVtw8Lm', 'user', 'siam.karlsson@elev.ga.ntig.se', '7eff3ea5f7e2e4e8f7b3355aff3a94be', 1);
 
 --
--- Indexes for dumped tables
+-- Index för dumpade tabeller
 --
 
 --
--- Indexes for table `bookings`
+-- Index för tabell `bookings`
 --
 ALTER TABLE `bookings`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `maintenance`
+-- Index för tabell `maintenance`
 --
 ALTER TABLE `maintenance`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `printers`
+-- Index för tabell `printers`
 --
 ALTER TABLE `printers`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `print_jobs`
+-- Index för tabell `print_jobs`
 --
 ALTER TABLE `print_jobs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `support_requests`
+-- Index för tabell `support_requests`
 --
 ALTER TABLE `support_requests`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `unavailable_dates`
+-- Index för tabell `unavailable_dates`
 --
 ALTER TABLE `unavailable_dates`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `date` (`date`);
 
 --
--- Indexes for table `users`
+-- Index för tabell `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT för dumpade tabeller
 --
 
 --
--- AUTO_INCREMENT for table `bookings`
+-- AUTO_INCREMENT för tabell `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT for table `maintenance`
+-- AUTO_INCREMENT för tabell `maintenance`
 --
 ALTER TABLE `maintenance`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `printers`
+-- AUTO_INCREMENT för tabell `printers`
 --
 ALTER TABLE `printers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `print_jobs`
+-- AUTO_INCREMENT för tabell `print_jobs`
 --
 ALTER TABLE `print_jobs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `support_requests`
---
-ALTER TABLE `support_requests`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `unavailable_dates`
+-- AUTO_INCREMENT för tabell `support_requests`
+--
+ALTER TABLE `support_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT för tabell `unavailable_dates`
 --
 ALTER TABLE `unavailable_dates`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT för tabell `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
